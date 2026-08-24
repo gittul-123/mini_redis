509756c7a8360d8427e65126f5334f4229a06933
gittul-123 <aiaidonno@gmail.com>
2026-08-24 15:53:31 +0900

    WIP : Implement min_heap

--- min_heap.py
+++ min_heap.py
@@ -25,3 +25,33 @@ class MinHeap:
 
             index = parent
             parent = (index - 1) // 2
+
+    def pop(self):
+        if self.heap == []:
+            return None
+        
+        min_value = self.heap[0]
+
+        self.heap[0] = self.heap[len(self.heap)-1]
+        self.heap.pop()
+        self._heapify_down(0)
+        return min_value
+    
+    def _heapify_down(self, index):
+        left = 2 * index + 1
+
+        while left < len(self.heap):
+            right = 2 * index + 2
+            smaller_child = left
+
+            if right < len(self.heap):
+                if self.heap[right] < self.heap[left]:
+                    smaller_child = right
+
+            if self.heap[index] > self.heap[smaller_child]:
+                # swap
+                # index 이동
+                # left 다시 계산
+
+            else:
+                break
